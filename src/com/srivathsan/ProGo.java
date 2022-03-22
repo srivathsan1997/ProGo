3086fd97156098f4ac829c00d9f1fbd8e558a708
srivathsan-8829 <srivathsan-8829@121.244.182.230>
2022-03-22 18:04:01 +0530

    Defining users and tasks class

--- src/com/srivathsan/ProGo.java
+++ src/com/srivathsan/ProGo.java
@@ -6,6 +6,7 @@ import java.util.Date;
 class Projects
 {
 	protected String projectName; //Name of the project
+	protected String projectId; //Identification Number for the project
 	protected String projectDescription; //Description for the project
 	protected ArrayList<Tasks> tasks = new ArrayList<Tasks>(); //A project contains number of tasks. Tasks in a project
 	protected ArrayList<Issues> issues = new ArrayList<Issues>(); //A project contains number of issues associated. Issues in a project
@@ -21,6 +22,14 @@ class Projects
 	public void setProjectName(String projectName) {
 		this.projectName = projectName;
 	}
+	
+	//Getter and Setter for projectId
+		public String getProjectId() {
+			return projectId;
+		}
+		public void setProjectId(String projectId) {
+			this.projectId = projectId;
+		}
 	
 	//Getter and Setter for projectDescription
 	public String getProjectDescription() {
@@ -39,7 +48,7 @@ class Projects
 	}
 	//Method to extract single task
 	public Tasks getTask(int index) {
-		return tasks.get(index);
+		return this.tasks.get(index);
 	}
 	
 	//Getter and Setter for issues
@@ -52,7 +61,7 @@ class Projects
 	//Method to extract single Issue
 	public Issues getIssue(int index)
 	{
-		return issues.get(index);
+		return this.issues.get(index);
 	}
 	
 	//Getter and Setter for users
@@ -65,7 +74,7 @@ class Projects
 	//Method to extract single user
 	public Users getUser(int index)
 	{
-		return users.get(index);
+		return this.users.get(index);
 	}
 	
 	//Getter and Setter for milestones
@@ -78,7 +87,7 @@ class Projects
 	//Method to extract single milestone
 	public Milestones getMilestone(int index)
 	{
-		return milestones.get(index);
+		return this.milestones.get(index);
 	}
 	
 	//Getter and Setter for startDate and endDate
@@ -95,15 +104,131 @@ class Projects
 		this.endDate = endDate;
 	}
 }
-class Issues
-{
-	
-}
 class Tasks
 {
+	protected String taskName; //Name of the tasks
+	protected String taskId; //Identification Number for the task
+	protected String taskDescription; //Description for the task
+	protected Users owner; //Owner of the task
+	protected Date startDate; //Task start date
+	protected Date dueDate; //Task end date
+	protected enum taskStatus{Assigned, Started, Delayed, Closed}; //taskStatus mentions if the task is just assigned or started or delayed or closed
+	protected enum priority{High,Medium,Low}; //taskPriority is to mention the priority of the task. It can be high, medium or low. 
+	protected Milestones milestone; //milestone to which the task is associated in a project
+	
+	//Getter and Setter for taskName
+	public String getTaskName() {
+		return taskName;
+	}
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	//Getter and Setter for taskId
+	public String getTaskId() {
+		return taskId;
+	}
+	public void setTaskId(String taskId) {
+		this.taskId = taskId;
+	}
+	
+	//Getter and Setter for taskDescription
+	public String getTaskDescription() {
+		return taskDescription;
+	}
+	public void setTaskDescription(String taskDescription) {
+		this.taskDescription = taskDescription;
+	}
+	
+	//Getter and Setter for taskOwner
+	public Users getOwner() {
+		return owner;
+	}
+	public void setOwner(Users owner) {
+		this.owner = owner;
+	}
+	
+	//Getter and Setter for startDate and dueDate
+	public Date getStartDate() {
+		return startDate;
+	}
+	public void setStartDate(Date startDate) {
+		this.startDate = startDate;
+	}
+	public Date getDueDate() {
+		return dueDate;
+	}
+	public void setDueDate(Date dueDate) {
+		this.dueDate = dueDate;
+	}
+	
+	//Getter and Setter for milestone
+	public Milestones getMilestone() {
+		return milestone;
+	}
+	public void setMilestone(Milestones milestone) {
+		this.milestone = milestone;
+	}
 	
 }
 class Users
+{
+	protected String memberName;
+	protected String memberId;
+	protected enum position{intern,employee,leader,manager};
+	protected ArrayList<Tasks> tasksAssigned;
+	protected ArrayList<Issues> issuesAssigned;
+	protected Milestones milestone;
+	
+	//Getter and Setter for memberName
+	public String getMemberName() {
+		return memberName;
+	}
+	public void setMemberName(String memberName) {
+		this.memberName = memberName;
+	}
+	
+	//Getter and Setter for memberId
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	
+	//Getter and Setter for tasksAssigned
+	public ArrayList<Tasks> getTasksAssigned() {
+		return tasksAssigned;
+	}
+	public void setTasksAssigned(ArrayList<Tasks> tasksAssigned) {
+		this.tasksAssigned = tasksAssigned;
+	}
+	//Method to extract single assigned task 
+	public Tasks getTask(int index) {
+		return tasksAssigned.get(index);
+	}
+	
+	//Getter and Setter for IssuesAssigned
+	public ArrayList<Issues> getIssuesAssigned() {
+		return issuesAssigned;
+	}
+	public void setIssuesAssigned(ArrayList<Issues> issuesAssigned) {
+		this.issuesAssigned = issuesAssigned;
+	}
+	//Method to extract single assigned issue
+	public Issues getIssueAssigned(int index) {
+		return issuesAssigned.get(index);
+	}
+	
+	//Getter and Setter for Milestone
+	public Milestones getMilestone() {
+		return milestone;
+	}
+	public void setMilestone(Milestones milestone) {
+		this.milestone = milestone;
+	}
+	
+}
+class teams
 {
 	
 }
@@ -111,7 +236,7 @@ class Milestones
 {
 	
 }
-class Teams
+class Issues
 {
 	
 }
